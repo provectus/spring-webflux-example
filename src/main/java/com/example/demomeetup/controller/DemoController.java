a710554bd4ee70c4790c58d25893f340b5ce4177
damirabdullin <dabdullin@provectus.com>
2021-06-19 23:42:19 -0700

    1s reactor

--- src/main/java/com/example/demomeetup/controller/DemoController.java
+++ src/main/java/com/example/demomeetup/controller/DemoController.java
@@ -5,9 +5,9 @@ import com.example.demomeetup.service.DemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.concurrent.Callable;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +16,8 @@ public class DemoController {
     private final DemoService demoService;
 
     @GetMapping("/flights")
-    public Callable<List<Flight>> flights() {
-        return demoService::getFlightsFromSlowService;
+    public Mono<List<Flight>> flights() {
+        return Mono.fromCallable(demoService::getFlightsFromSlowService);
     }
 
     @GetMapping("/fast")
