4adf4a27f7806ea987de21df99d3d407ef7d8196
damirabdullin <dabdullin@provectus.com>
2021-06-20 17:53:33 -0700

    comment

--- src/main/java/com/example/demomeetup/controller/DemoController.java
+++ src/main/java/com/example/demomeetup/controller/DemoController.java
@@ -16,9 +16,9 @@ public class DemoController {
 
     @GetMapping("/flights")
     public List<Flight> flights() {
-        long start = System.currentTimeMillis();
+        //long start = System.currentTimeMillis();
         var result = demoService.getFlightsFromSlowService();
-        System.out.println("Execution time: " + (System.currentTimeMillis() - start));
+        //System.out.println("Execution time: " + (System.currentTimeMillis() - start));
         return result;
     }
 
